eda238b8c8a200cd69636649e1188d432eccee2f
Mustapha Abdelaziz <aziz.mus.19@gmail.com>
2021-10-11 07:47:44 +0100

    update

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
 </template>
 
 <style lang="scss">
-// @import "~@/assets/scss/vendors/bootstrap-vue/index";
+@import "~@/assets/scss/vendors/bootstrap-vue/index";
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
